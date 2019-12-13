48e52468ff19991619ccbbf7654de8f818444c37
coardeesh00k3 <57509150+coardeesh00k3@users.noreply.github.com>
2019-12-12 16:05:11 -0800

    12/12/19

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
 class HelloWorld
-{    public static void main (String[]orgs)
-    {    System.out.println("HelloWorld");
+{   
+    public static void main (String[]args)
+    {   
+        Scanner s= new Scanner(System.in);
+        System.out.println("Get a sword?(y/n)");
+        char ans = s.next().charAt(0);
+        boolean hasSword;
+        hasSword=true;
+        if (hasSword)
+        {
+            System.out.println("You have a sword");
+            System.out.println("Good luck");
+        } else 
+        {
+            System.out.println("You do not have a sword");
+            System.out.println("Good luck with your bare hands");
+        }
+        System.out.println("The End");
     }
 }
